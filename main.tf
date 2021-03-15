69945fc9de2b4760260a137c00b8a11a26338206
Anthony Pack <48550732+anthonyk-pack@users.noreply.github.com>
2021-03-15 14:03:40 +0000

    add ec2 deployment

--- main.tf
+++ main.tf
@@ -2,36 +2,132 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-variable "cidr_blocks" {
-    description = "cidr blocks and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
 
     tags = {
-        Name = var.cidr_blocks[0].name
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "eu-west-2a"
-
-     tags = {
-        Name = var.cidr_blocks[1].name
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
 
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
 }
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+        Name = "${var.env_prefix}-default-sg"
+    }
+}
+
+/* This section calls out to AWS to pull latest version of the amazon image we are using - referenced from EC2 VM gallery in AWS */
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+/* This output will query aws for values of the EC2 instance we defined above. Add ID suffix if its just the ID you want to output
+output "aws_ami" {
+    value = data.aws_ami.latest-amazon-linux-image
+}*/
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = var.public_key
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+/* Option 1 - Below script installs docker, starts docker, promotes the local ec2 user and runs the docker command. This only executes once
+    user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                EOF */
+/* Option 2 - Call an entry script with the code*/
+
+    user_data = file("entry-script.sh")
+
+    tags = {
+        name = "${var.env_prefix}-server"
+    }
 }
